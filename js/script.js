8eef541729d774d6c3861bcb20c150a164daf07b
Veronika <veronikakuchtova95@gmail.com>
2020-10-28 21:46:52 +0100

    Need to fix the "total score" part

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-// Generate 16 unique random numbers from 1 to 100
+// Variables
 var bombNumbers = [];
+var guessed = [];
+var score = 0;
+
+// Generate 16 unique random numbers from 1 to 100
 for (var i = 0; i < 16; i++){
   var bomb = generateNumber(100);
   if (bombNumbers.includes(bomb)) {
@@ -9,32 +13,21 @@ for (var i = 0; i < 16; i++){
     bombNumbers.push(bomb);
   }
 }
-console.log("Bomb numbers: ", bombNumbers;
+console.log("Bomb numbers: ", bombNumbers);
 
 // Ask a number from 1 to 100.
-var guessed = [];
-var score = 0;
-var guess = parseInt(prompt('Inserisci un numero da 1 a 100'));
-guessed.push(guess);
-// If the inputted number is among the numbers generated earlier, the user loses...
 for (var i = 0; i < 83; i++) {
+  var guess = parseInt(prompt('Inserisci un numero da 1 a 100'));
+  var goodGuess = verify(guess, guessed);
+  // If the inputted number is among the numbers generated earlier, the user loses...
   if (bombNumbers.includes(guess)) {
-    // At the end of the game, tell the user's total score
+    //User's total score
     console.log('You lose, score: ', score);
     break;
   }
   // ...If not, continue asking numbers.
   else {
-    var guess = parseInt(prompt('Inserisci un numero da 1 a 100'));
-    // Can't guess the same number twice
-    if (guessed.includes(guess)) {
-      i--;
-      var alert = alert("Non puoi inserire lo stesso numero più di una volta")
-    }
-    else {
-      guessed.push(guess);
-      score = score + 1;
-    }
+    score = score + 1;
   }
 }
 console.log("Guessed numbers: ", guessed);
@@ -43,4 +36,16 @@ console.log("Guessed numbers: ", guessed);
 // Functions
 function generateNumber(max) {
   return Math.ceil(Math.random() * max);
+}
+
+function verify(inpt, arr) {
+  if (isNaN(inpt)) {
+    var not = alert("Devi inserire un numero da 1 a 100.");
+  }
+  else if (guessed.includes(inpt)) {
+    not = alert("Il numero non può essere scritto più di una volta");
+  }
+  else {
+    arr.push(inpt);
+  }
 }
