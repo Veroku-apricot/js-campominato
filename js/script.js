e387ff88bcc45d88782b382ab501e77613054025
Veronika <veronikakuchtova95@gmail.com>
2020-10-28 20:24:24 +0100

    Almost done

--- js/script.js
+++ js/script.js
@@ -1,21 +1,37 @@
 // Generate 16 unique random numbers from 1 to 100
-var bomb = "";
+var bombNumbers = [];
 for (var i = 0; i < 16; i++){
-  var n = generateNumber(1, 100);
-  bomb = bomb + n + " ";
+  var bomb = generateNumber(100);
+  if (bombNumbers.includes(bomb)) {
+    i--;
+  }else {
+    bombNumbers.push(bomb);
+  }
 }
-console.log(bomb);
+console.log("Bomb numbers: ", bombNumbers);
 
 // Ask 84 times a number from 1 to 100. Can't repeat the same number.
+var guessed = [];
+var score = 0;
 var guess = parseInt(prompt('Inserisci un numero da 1 a 100'));
-console.log(guess);
-
+guessed.push(guess);
 // If the inputted number is among the numbers generated earlier, the user loses. If not, continue asking numbers.
+for (var i = 0; i < 83; i++) {
+  if (bombNumbers.includes(guess)) {
+    // At the end of the game, tell the user's total score
+    console.log('You lose, score: ', score);
+    break;
+  }else {
+    var guess = parseInt(prompt('Inserisci un numero da 1 a 100'));
+    guessed.push(guess);
+    score = score + 1;
+  }
 
-// At the end of the game, tell the user's total score
+}
+console.log("Guessed numbers: ", guessed);
 
 
 // Functions
-function generateNumber(min, max) {
-  return Math.ceil(Math.random() * (max - min));
+function generateNumber(max) {
+  return Math.ceil(Math.random() * max);
 }
